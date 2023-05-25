667dcea80c497878987aeaee2086fe3501c6632b
Elisey <elisei2508@gmail.com>
2023-05-25 19:37:20 +0300

    Upd6

--- core/src/com/distgdx/game/Player.java
+++ core/src/com/distgdx/game/Player.java
@@ -1,10 +1,24 @@
 package com.distgdx.game;
 
+import static com.distgdx.game.MyGame.*;
+
+import com.badlogic.gdx.graphics.Texture;
+
 public class Player {
     String name;
     long time;
+    float x, y, width, height;
+    int phase = 2;
+    float vx, vy;
+    boolean isAlive = true;
+    Texture imgLive, imgDead;
 
     public Player(String name) {
         this.name = name;
     }
+    void Move(){
+        x = 144 + height;
+
+    }
+
 }
